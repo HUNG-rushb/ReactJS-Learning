3e910f324124404ac4f2931f2d2ea16a03963e9c
unknown <tdhtp2016@gmail.com>
2021-12-21 22:53:48 +0700

    ok

--- 18. Redux/01-starting-project/src/index.js
+++ 18. Redux/01-starting-project/src/index.js
@@ -1,53 +1,16 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-// import store from "./store/index";
+import store from "./store/index";
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
-import fetch from "node-fetch";
-//const fetch = require('node-fetch');
-
-const postID = "5122874051130134"; // 'https://www.facebook.com/LienMinhHuyenThoai/posts/4387428904674656'
-const accessToken = ""; // find 'EAAA' at 'https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed'
-const url =
-  "https://graph.facebook.com/" +
-  postID +
-  "/comments?fields=message&access_token=" +
-  accessToken;
-const analyze_comments = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((results) => {
-      if (results) {
-        const comments = results.data;
-        comments.forEach((comment) => {
-          const message = comment.message.trim();
-          if (message.length > 0 && message.includes("LOL")) {
-            let startIndex = message.indexOf("LOL");
-            let code = message.substr(startIndex, 13);
-            console.log(code);
-          }
-        });
-        if (typeof results.paging.next !== "undefined") {
-          analyze_comments(results.paging.next);
-        } else {
-          console.log("Done!");
-        }
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-analyze_comments(url);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
