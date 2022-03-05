477843968bdc8b4a5b8e49bf59ee1683b181cb5f
unknown <tdhtp2016@gmail.com>
2022-03-05 08:45:20 +0700

    ok

--- 20. React Router/01-starting-project/src/App.js
+++ 20. React Router/01-starting-project/src/App.js
@@ -1,7 +1,19 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/* <h2>Let's get started!</h2> */}
+      <Route path="/welcome">
+        <Welcome></Welcome>
+      </Route>
+
+      <Route path="/welcome">
+        <Products></Products>
+      </Route>
     </div>
   );
 }
